d2873be1ff75af85be8e040527845a177031d0db
julianashwin <julianashwin@gmail.com>
2022-03-18 17:28:27 +0100

    change siler fn and add I(2)

--- src/helpers.jl
+++ src/helpers.jl
@@ -71,7 +71,8 @@ end
 """
 Function that creates summary dataframes for each Siler parameter
 """
-function extract_variables(chain_in, years; log_pars = false, σ_pars = true, ext = false)
+function extract_variables(chain_in, years; log_pars = false, σ_pars = true, ext = false,
+        firstdiff = false)
     # Names
     if log_pars
         if ext
@@ -117,7 +118,7 @@ function extract_variables(chain_in, years; log_pars = false, σ_pars = true, ex
 
     # If we have a dynamic model with variance terms for the parameters, add these
     if σ_pars
-        if ext
+        if firstdiff
             # Parameter time series variance
             σ_par_names = vcat([:iteration, :chain], Symbol.("σ_par[".*string.(1:6).*"]"))
             σ_pars_ests = summarise_stats(df_indep[:,σ_par_names], stats[in.(stats.parameters, [σ_par_names]),:],
@@ -146,6 +147,44 @@ function extract_variables(chain_in, years; log_pars = false, σ_pars = true, ex
             #τ_pars_ests.parameter = [:τ_B, :τ_b, :τ_C, :τ_c, :τ_d, :τ_σ]
             #τ_pars_ests.year .= 0
             #par_ests = vcat(par_ests, τ_pars_ests)
+        elseif ext
+            σ_par_names = vcat([:iteration, :chain], Symbol.("σ_par[".*string.(1:6).*"]"))
+            σ_pars_ests = summarise_stats(df_indep[:,σ_par_names], stats[in.(stats.parameters, [σ_par_names]),:],
+                Int.(1:6), log_pars = false, parname = :σ_par)
+            σ_pars_ests.parameter = [:σ_B, :σ_b, :σ_C, :σ_c, :σ_d, :σ_σ]
+            σ_pars_ests.year .= 0
+
+            par_ests = vcat(par_ests, σ_pars_ests)
+
+            σ_α_par_names = vcat([:iteration, :chain], Symbol.("σ_α".*["B", "b", "C", "c", "d", "σ"]))
+            σ_α_pars_ests = summarise_stats(df_indep[:,(σ_α_par_names)], stats[in.(stats.parameters, [σ_α_par_names]),:],
+                Int.(1:6), log_pars = false, parname = :σ_α_par)
+            σ_α_pars_ests.parameter = [:σ_αB, :σ_αb, :σ_αC, :σ_αc, :σ_αd, :σ_ασ]
+            σ_α_pars_ests.year .= 0
+
+            par_ests = vcat(par_ests, σ_α_pars_ests)
+
+            α_Bs = vcat([:iteration, :chain], Symbol.("α_B[".*string.(1:length(years)-1).*"]"))
+            α_bs = vcat([:iteration, :chain], Symbol.("α_b[".*string.(1:length(years)-1).*"]"))
+            α_Cs = vcat([:iteration, :chain], Symbol.("α_C[".*string.(1:length(years)-1).*"]"))
+            α_cs = vcat([:iteration, :chain], Symbol.("α_c[".*string.(1:length(years)-1).*"]"))
+            α_ds = vcat([:iteration, :chain], Symbol.("α_d[".*string.(1:length(years)-1).*"]"))
+            α_σs = vcat([:iteration, :chain], Symbol.("α_σ[".*string.(1:length(years)-1).*"]"))
+
+            α_B_ests = summarise_stats(df_indep[:,α_Bs], stats[in.(stats.parameters, [α_Bs]),:],
+                years[2:end], log_pars = false, parname = :α_B)
+            α_b_ests = summarise_stats(df_indep[:,α_bs], stats[in.(stats.parameters, [α_bs]),:],
+                years[2:end], log_pars = false, parname = :α_b)
+            α_C_ests = summarise_stats(df_indep[:,α_Cs], stats[in.(stats.parameters, [α_Cs]),:],
+                years[2:end], log_pars = false, parname = :α_C)
+            α_c_ests = summarise_stats(df_indep[:,α_cs], stats[in.(stats.parameters, [α_cs]),:],
+                years[2:end], log_pars = false, parname = :α_c)
+            α_d_ests = summarise_stats(df_indep[:,α_ds], stats[in.(stats.parameters, [α_ds]),:],
+                years[2:end], log_pars = false, parname = :α_d)
+            α_σ_ests = summarise_stats(df_indep[:,α_σs], stats[in.(stats.parameters, [α_σs]),:],
+                years[2:end], log_pars = false, parname = :α_σ)
+
+            par_ests = vcat(par_ests, α_B_ests, α_b_ests, α_C_ests, α_c_ests, α_d_ests, α_σ_ests)
 
         else
             σ_par_names = vcat([:iteration, :chain], Symbol.("σ_pars[".*string.(1:6).*"]"))
--- src/models.jl
+++ src/models.jl
@@ -13,7 +13,7 @@ Static Siler model on non-logged data
     # Our prior beliefs
     B ~ LogNormal(log(10), 2.0)
     b ~ LogNormal(log(2), 1.0)
-    C ~ LogNormal(log(120), 2.0)
+    C ~ LogNormal(log(10), 2.0)
     c ~ LogNormal(log(0.1), 1.0)
     d ~ LogNormal(log(0.025), 1.0)
     σ ~ LogNormal(log(0.001), 1.0)
@@ -25,7 +25,7 @@ Static Siler model on non-logged data
     ld = log(d)
     lσ = log(σ)
     # Find mean using the siler mortality function
-    μs = exp.(- exp(lb).* (ages .+ exp(lB))) .+ exp.(exp(lc) .* (ages .- exp(lC))) .+ exp(ld)
+    μs = exp.(- exp(lb).* ages .+ exp(lB)) .+ exp.(exp(lc) .* ages .- exp(lC)) .+ exp(ld)
     m_var = exp(σ)
     #m_vars[m_vars.<= 1e-10] .= 1e-10
     # Draw from normal dist
@@ -58,7 +58,7 @@ Static Siler model on log data
     ld = log(d)
     lσ = log(σ)
     # Find mean using the siler mortality function
-    μs = exp.(- exp(lb).* (ages .+ exp(lB))) .+ exp.(exp(lc) .* (ages .- exp(lC))) .+ exp(ld)
+    μs = exp.(- exp(lb).* ages .+ exp(lB)) .+ exp.(exp(lc) .* ages .- exp(lC)) .+ exp(ld)
     m_vars = exp(σ).*ones(N)
     #m_vars[m_vars.<= 1e-10] .= 1e-10
     # Variance matrix
@@ -83,7 +83,7 @@ Multiple independent Siler models
     # Our prior beliefs
     B ~ filldist(LogNormal(log(10), 2.0), T)
     b ~ filldist(LogNormal(log(2), 1.0), T)
-    C ~ filldist(LogNormal(log(120), 2.0), T)
+    C ~ filldist(LogNormal(log(10), 2.0), T)
     c ~ filldist(LogNormal(log(0.1), 1.0), T)
     d ~ filldist(LogNormal(log(0.025), 1.0), T)
     σ ~ filldist(LogNormal(log(0.1), 1.0), T)
@@ -97,8 +97,8 @@ Multiple independent Siler models
 
     for tt in 1:T
         # Find mean using the siler mortality function
-        μs = exp.(-exp(lb[tt]).*(ages .+ exp(lB[tt]))) .+
-            exp.(exp(lc[tt]).*(ages .- exp(lC[tt]))) .+ exp(ld[tt])
+        μs = exp.(-exp(lb[tt]).*ages .+ exp(lB[tt])) .+
+            exp.(exp(lc[tt]).*ages .- exp(lC[tt])) .+ exp(ld[tt])
         lm_vars = exp(lσ[tt]).*ones(N)
         lm_vars[lm_vars.<= 1e-10] .= 1e-10
         # Variance matrix
@@ -142,13 +142,13 @@ Dynamic Siler model with random walk in log parameters
     # First period from priors
     lB[1] ~ Normal(log(10), 2.0)
     lb[1] ~ Normal(log(2), 1.0)
-    lC[1] ~ Normal(log(120), 2.0)
+    lC[1] ~ Normal(log(10), 2.0)
     lc[1] ~ Normal(log(0.1), 1.0)
     ld[1] ~ Normal(log(0.025), 1.0)
     lσ[1] ~ Normal(log(0.1), 1.0)
     # Find mean using the siler mortality function
-    μs = exp.(-exp(lb[1]).*(ages .+ exp(lB[1]))) .+
-        exp.(exp(lc[1]).*(ages .- exp(lC[1]))) .+ exp(ld[1])
+    μs = exp.(-exp(lb[1]).*ages .+ exp(lB[1])) .+
+        exp.(exp(lc[1]).*ages .- exp(lC[1])) .+ exp(ld[1])
     lm_vars = exp(lσ[1]).*ones(N)
     lm_vars[lm_vars.<= 1e-10] .= 1e-10
     # Variance matrix
@@ -174,8 +174,8 @@ Dynamic Siler model with random walk in log parameters
         ld[tt] ~ Normal(ld[tt-1], var_d)
         lσ[tt] ~ Normal(lσ[tt-1], var_σ)
         # Find mean using the siler mortality function
-        μs = exp.(-exp(lb[tt]).*(ages .+ exp(lB[tt]))) .+
-            exp.(exp(lc[tt]).*(ages .- exp(lC[tt]))) .+ exp(ld[tt])
+        μs = exp.(-exp(lb[tt]).*ages .+ exp(lB[tt])) .+
+            exp.(exp(lc[tt]).*ages .- exp(lC[tt])) .+ exp(ld[tt])
         lm_vars = exp(lσ[tt]).*ones(N)
         lm_vars[lm_vars.<= 1e-10] .= 1e-10
         # Variance matrix
@@ -224,13 +224,13 @@ Note that mean of InverseWishart is S/(m - p - 1) where S is scale matrix, m is 
     for tt in 1:2
         lB[tt] ~ Normal(log(10), 2.0)
         lb[tt] ~ Normal(log(2), 1.0)
-        lC[tt] ~ Normal(log(120), 2.0)
+        lC[tt] ~ Normal(log(10), 2.0)
         lc[tt] ~ Normal(log(0.1), 1.0)
         ld[tt] ~ Normal(log(0.025), 1.0)
         lσ[tt] ~ Normal(log(0.1), 1.0)
         # Find mean using the siler mortality function
-        μs = exp.(-exp(lb[tt]).*(ages .+ exp(lB[tt]))) .+
-            exp.(exp(lc[tt]).*(ages .- exp(lC[tt]))) .+ exp(ld[tt])
+        μs = exp.(-exp(lb[tt]).*ages .+ exp(lB[tt])) .+
+            exp.(exp(lc[tt]).*ages .- exp(lC[tt])) .+ exp(ld[tt])
         lm_vars = exp(lσ[tt]).*ones(N)
         lm_vars[lm_vars.<= 1e-10] .= 1e-10
         # Variance matrix
@@ -264,8 +264,8 @@ Note that mean of InverseWishart is S/(m - p - 1) where S is scale matrix, m is 
         lσ[tt] ~ Normal(μ_σ, var_σ)
 
         # Find mean using the siler mortality function
-        μs = exp.(-exp(lb[tt]).*(ages .+ exp(lB[tt]))) .+
-            exp.(exp(lc[tt]).*(ages .- exp(lC[tt]))) .+ exp(ld[tt])
+        μs = exp.(-exp(lb[tt]).*ages .+ exp(lB[tt])) .+
+            exp.(exp(lc[tt]).*ages .- exp(lC[tt])) .+ exp(ld[tt])
         lm_vars = exp(lσ[tt]).*ones(N)
         lm_vars[lm_vars.<= 1e-10] .= 1e-10
         # Variance matrix
@@ -285,7 +285,7 @@ end
 
 """
 Dynamic Siler model with extensions
-    - drift term
+    - time-varying drift term
     - Non-diagonal covariance matrix (not working)
     - AR(1) with trend (not working)
 
@@ -305,15 +305,24 @@ Note that mean of InverseWishart is S/(m - p - 1) where S is scale matrix, m is 
     # Priors on variance terms for parameter time series
     σ_par ~ filldist(InverseGamma(2, 0.05),6)
     # Correlation matrix of shocks to parameter time series
-    #ρ_block ~ LKJ(6, 4)
-    # Prior on variance terms for parameter time series
-    #Σ_ϵ = Diagonal(σ_par)#quad_form_diag(ρ_block, σ_par)
     # Priors on constant
-    α_pars ~ filldist(Normal(0., 0.05), 6)
+    α_B = Vector(undef, T-1)
+    α_b = Vector(undef, T-1)
+    α_C = Vector(undef, T-1)
+    α_c = Vector(undef, T-1)
+    α_d = Vector(undef, T-1)
+    α_σ = Vector(undef, T-1)
+    # Variance of drift random walk
+    σ_αB ~ InverseGamma(2, 0.01)
+    σ_αb ~ InverseGamma(2, 0.01)
+    σ_αC ~ InverseGamma(2, 0.01)
+    σ_αc ~ InverseGamma(2, 0.01)
+    σ_αd ~ InverseGamma(2, 0.01)
+    σ_ασ ~ InverseGamma(2, 0.01)
     # Priors on time trend terms
     #τ_pars ~ filldist(Normal(0, 0.05), 6)
     # Autoregressive coefficient
-    #β_pars ~ filldist(Uniform(0.0, 0.99), 6)
+    #β_pars ~ filldist(Uniform(0.0, 0.99), 6
     # First period from priors
     lB[1] ~ Normal(log(10), 2.0)
     lb[1] ~ Normal(log(2), 1.0)
@@ -322,14 +331,24 @@ Note that mean of InverseWishart is S/(m - p - 1) where S is scale matrix, m is 
     ld[1] ~ Normal(log(0.025), 1.0)
     lσ[1] ~ Normal(log(0.1), 1.0)
     # Find mean using the siler mortality function
-    μs = exp.(-exp(lb[1]).*(ages .+ exp(lB[1]))) .+
-        exp.(exp(lc[1]).*(ages .- exp(lC[1]))) .+ exp(ld[1])
+    μs = exp.(-exp(lb[1]).*ages .+ exp(lB[1])) .+
+        exp.(exp(lc[1]).*ages .- exp(lC[1])) .+ exp(ld[1])
     lm_vars = exp(lσ[1]).*ones(N)
     lm_vars[lm_vars.<= 1e-10] .= 1e-10
     # Variance matrix
     Σ_m = Diagonal(lm_vars)
     # Draw from truncated normal dist
     lm_data[1] ~ MvNormal(log.(μs), Σ_m)
+
+    # First period drift terms
+    #β_pars ~ filldist(Uniform(0.0, 0.99), 6
+    α_B[1] ~ Normal(0., 0.02)
+    α_b[1] ~ Normal(0., 0.02)
+    α_C[1] ~ Normal(0., 0.02)
+    α_c[1] ~ Normal(0., 0.02)
+    α_d[1] ~ Normal(0., 0.02)
+    α_σ[1] ~ Normal(0., 0.02)
+
     # Loop through random walk process
     for tt in 2:T
         # Calculate updated parameter means
@@ -339,12 +358,12 @@ Note that mean of InverseWishart is S/(m - p - 1) where S is scale matrix, m is 
         #μ_c = α_pars[4] + β_pars[4]*lc[tt-1] + τ_pars[4]*(tt-1)
         #μ_d = α_pars[5] + β_pars[5]*ld[tt-1] + τ_pars[5]*(tt-1)
         #μ_σ = α_pars[6] + β_pars[6]*lσ[tt-1] + τ_pars[6]*(tt-1)
-        μ_B = α_pars[1] + lB[tt-1]
-        μ_b = α_pars[2] + lb[tt-1]
-        μ_C = α_pars[3] + lC[tt-1]
-        μ_c = α_pars[4] + lc[tt-1]
-        μ_d = α_pars[5] + ld[tt-1]
-        μ_σ = α_pars[6] + lσ[tt-1]
+        μ_B = α_B[tt-1] + lB[tt-1]
+        μ_b = α_b[tt-1] + lb[tt-1]
+        μ_C = α_C[tt-1] + lC[tt-1]
+        μ_c = α_c[tt-1] + lc[tt-1]
+        μ_d = α_d[tt-1] + ld[tt-1]
+        μ_σ = α_σ[tt-1] + lσ[tt-1]
         # Updated variance parameters for the mortality curve
         var_B = max(σ_par[1], 1e-8)
         var_b = max(σ_par[2], 1e-8)
@@ -361,14 +380,24 @@ Note that mean of InverseWishart is S/(m - p - 1) where S is scale matrix, m is 
         lσ[tt] ~ Normal(μ_σ, var_σ)
 
         # Find mean using the siler mortality function
-        μs = exp.(-exp(lb[tt]).*(ages .+ exp(lB[tt]))) .+
-            exp.(exp(lc[tt]).*(ages .- exp(lC[tt]))) .+ exp(ld[tt])
+        μs = exp.(-exp(lb[tt]).*ages .+ exp(lB[tt])) .+
+            exp.(exp(lc[tt]).*ages .- exp(lC[tt])) .+ exp(ld[tt])
         lm_vars = exp(lσ[tt]).*ones(N)
         lm_vars[lm_vars.<= 1e-10] .= 1e-10
         # Variance matrix
         Σ_m = Diagonal(lm_vars)
         # Draw from truncated normal dist
         lm_data[tt] ~ MvNormal(log.(μs), Σ_m)
+
+        # Update the r.w. with drift term
+        if tt < T
+            α_B[tt] ~ Normal(α_B[tt-1], σ_αB)
+            α_b[tt] ~ Normal(α_b[tt-1], σ_αb)
+            α_C[tt] ~ Normal(α_C[tt-1], σ_αC)
+            α_c[tt] ~ Normal(α_c[tt-1], σ_αc)
+            α_d[tt] ~ Normal(α_d[tt-1], σ_αd)
+            α_σ[tt] ~ Normal(α_σ[tt-1], σ_ασ)
+        end
     end
 end
 
